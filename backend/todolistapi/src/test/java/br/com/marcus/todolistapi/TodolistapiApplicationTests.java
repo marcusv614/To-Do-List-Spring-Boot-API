de88f3eaa8534a0bb9b15965c6141c0eb8288305
Marcus (Desktop/IronBridge) <marcus.pinheiro@super.ufam.edu.br>
2026-02-21 12:51:05 -0400

    feat:cria pastas frontend e backend e inicia projeto

---
+++ backend/todolistapi/src/test/java/br/com/marcus/todolistapi/TodolistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcus.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
