4a1892876b676fce2a8fdf037043e0b2146a4d6c
Marcus Pinheiro <marcus.pinheiro@super.ufam.edu.br>
2026-02-23 13:56:48 -0400

    feat: cria endpoint DELETE (#27)

--- backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
+++ backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
@@ -1,8 +1,10 @@
 package br.com.marcus.todolistapi.controller;
 
 import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.marcus.todolistapi.model.TaskModel;
 import br.com.marcus.todolistapi.service.TaskService;
 
@@ -35,6 +38,11 @@ public class TaskController {
     @PutMapping("/{id}")
     public ResponseEntity update(@RequestBody TaskModel model, @PathVariable Long id){
         service.updateTask(id, model);
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        service.deleteTask(id);
         return ResponseEntity.ok().build();
     }
 }
--- backend/todolistapi/src/main/java/br/com/marcus/todolistapi/service/TaskService.java
+++ backend/todolistapi/src/main/java/br/com/marcus/todolistapi/service/TaskService.java
@@ -1,7 +1,9 @@
 package br.com.marcus.todolistapi.service;
 
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+
 import br.com.marcus.todolistapi.model.TaskModel;
 import br.com.marcus.todolistapi.repository.TaskRepository;
 
@@ -26,5 +28,10 @@ public class TaskService {
         .orElseThrow(() -> new RuntimeException("Task not found"));
         modelData.setTitle(model.getTitle());
         repo.save(modelData);
+    }
+    public void deleteTask(Long id){
+        TaskModel model = repo.findById(id)
+        .orElseThrow(() -> new RuntimeException("User not found"));
+        repo.delete(model);
     }
 }
