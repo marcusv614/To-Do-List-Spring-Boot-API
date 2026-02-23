f9fbb1807c86247af0d6d80dc038c974945535ec
Marcus (Desktop/IronBridge) <marcus.pinheiro@super.ufam.edu.br>
2026-02-23 14:06:16 -0400

    feat: cors config
    autoriza a porta 5173 para fazer requisições
    
    Closes #17

--- backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
+++ backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
@@ -1,9 +1,9 @@
 package br.com.marcus.todolistapi.controller;
 
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,13 +12,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.marcus.todolistapi.model.TaskModel;
 import br.com.marcus.todolistapi.service.TaskService;
 
 @RestController
 @RequestMapping("/todo")
-public class TaskController {
+@CrossOrigin(origins = "http://localhost:5173")public class TaskController {
     private final TaskService service;
 
     public TaskController(TaskService service){
