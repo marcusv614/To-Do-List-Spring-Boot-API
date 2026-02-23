f00fcd0ac68d37dd83944171e68fbfaec8d012d9
Marcus Pinheiro <marcus.pinheiro@super.ufam.edu.br>
2026-02-23 10:16:54 -0400

    feature/refactor: cria endpoint PUT e altera o nome de alguns métodos (#26)

--- backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
+++ backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +23,18 @@ public class TaskController {
     }
 
     @GetMapping
-    public ResponseEntity<List<TaskModel>> showAllTasks(){
-        return ResponseEntity.ok(service.show());
+    public ResponseEntity<List<TaskModel>> show(){
+        return ResponseEntity.ok(service.showAllTasks());
     }
 
     @PostMapping
-    public ResponseEntity saveTask(@RequestBody TaskModel model){
+    public ResponseEntity save(@RequestBody TaskModel model){
         service.saveTaskModel(model);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel model, @PathVariable Long id){
+        service.updateTask(id, model);
+        return ResponseEntity.ok().build();
     }
 }
