b66e0615c4637633219093d0d3a9caaf82c02f49
Marcus (Desktop/IronBridge) <marcus.pinheiro@super.ufam.edu.br>
2026-02-24 16:56:09 -0400

    feat: cria TaskItem

--- frontend/todolist-frontend/src/components/TaskItem.jsx
+++ frontend/todolist-frontend/src/components/TaskItem.jsx
@@ -1,1 +1,19 @@
-    
+import { deleteTask } from "../services/TaskService";
+
+export default function TaskItem({ task, onTaskDeleted }) {
+  const handleDelete = async () => {
+    try {
+      await deleteTask(task.id);
+      onTaskDeleted(task.id);
+    } catch (error) {
+      console.error("Erro ao deletar:", error);
+    }
+  };
+
+  return (
+    <li>
+      {task.title}
+      <button onClick={handleDelete}>Excluir</button>
+    </li>
+  );
+}
