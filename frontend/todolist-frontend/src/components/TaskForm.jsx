9903289d2e1cb529ecb22dab77e702a24ea34a66
Marcus Pinheiro <marcus.pinheiro@super.ufam.edu.br>
2026-02-24 14:37:13 -0400

    feat: cria taskform (#37)
    Closes #35

---
+++ frontend/todolist-frontend/src/components/TaskForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { postTasks } from "../services/TaskService";
+
+export default function TaskForm({ onTaskCreated }) {
+  const [title, setTitle] = useState("");
+
+  const sendForms = async (e) => {
+    e.preventDefault();
+
+    if (!title.trim()) return;
+
+    try {
+      const response = await postTasks({ title });
+      onTaskCreated(response.data);
+      setTitle("");
+    } catch (error) {
+      console.log("Erro ao criar task");
+    }
+  };
+
+  return (
+    <form onSubmit={sendForms}>
+      <input
+        type="text"
+        placeholder="Digite sua task"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+}
