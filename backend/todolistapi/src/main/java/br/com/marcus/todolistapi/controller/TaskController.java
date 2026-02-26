3f0d72743d9e4eaab135bfac74eda8662fd8142b
Marcus (Desktop/IronBridge) <marcus.pinheiro@super.ufam.edu.br>
2026-02-25 20:11:19 -0400

    teste cors

--- backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
+++ backend/todolistapi/src/main/java/br/com/marcus/todolistapi/controller/TaskController.java
@@ -1,6 +1,7 @@
 package br.com.marcus.todolistapi.controller;
 
 import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.marcus.todolistapi.model.TaskModel;
 import br.com.marcus.todolistapi.service.TaskService;
 
 @RestController
 @RequestMapping("/todo")
-@CrossOrigin(origins = "*") 
+@CrossOrigin(origins = "https://to-do-list-six-azure.vercel.app/") 
 public class TaskController {
     private final TaskService service;
 
